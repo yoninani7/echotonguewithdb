21185f21c9c2052714a7226dab145a777a1d90ee
yoninani7 <yonsbreak7@gmail.com>
2026-01-09 14:25:39 +0300

    Update index.php

--- index.php
+++ index.php
@@ -665,7 +665,7 @@ function sendJsonResponse($response) {
         <a href="index.html" class="cinzel">
             Echotongue
         </a>
-        <a href="echotongue.dsintertravel.com" class="back-home">
+        <a href="https://echotongue.dsintertravel.com" class="back-home">
             <i class="fas fa-arrow-left"></i>
             <span>Back to Main Site</span>
         </a>
